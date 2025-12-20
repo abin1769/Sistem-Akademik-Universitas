607d316825f54765aad800b327cb5af0e64a3807
abin1769 <abin.shafwan@gmail.com>
2025-12-20 22:07:52 +0700

    menambah template sql

---
+++ siak.sql
@@ -0,0 +1,378 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 20 Des 2025 pada 16.06
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `siak`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `admin`
+--
+
+CREATE TABLE `admin` (
+  `id_admin` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `username` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `admin`
+--
+
+INSERT INTO `admin` (`id_admin`, `id_user`, `username`) VALUES
+(1, 1, 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `dosen`
+--
+
+CREATE TABLE `dosen` (
+  `id_dosen` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `nidn` varchar(30) NOT NULL,
+  `departemen` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `dosen`
+--
+
+INSERT INTO `dosen` (`id_dosen`, `id_user`, `nidn`, `departemen`) VALUES
+(1, 2, '12345678', 'Informatika'),
+(2, 3, '87654321', 'Sistem Informasi');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `krs`
+--
+
+CREATE TABLE `krs` (
+  `id_krs` int(11) NOT NULL,
+  `id_mahasiswa` int(11) NOT NULL,
+  `semester` int(11) NOT NULL,
+  `tahun_ajaran` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `krs`
+--
+
+INSERT INTO `krs` (`id_krs`, `id_mahasiswa`, `semester`, `tahun_ajaran`) VALUES
+(1, 1, 1, '2025/2026'),
+(2, 2, 1, '2025/2026'),
+(3, 1, 1, '2025/2026'),
+(4, 1, 1, '2025/2026'),
+(5, 1, 1, '2025/2026');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `krs_detail`
+--
+
+CREATE TABLE `krs_detail` (
+  `id_krs_detail` int(11) NOT NULL,
+  `id_krs` int(11) NOT NULL,
+  `id_mk` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `krs_detail`
+--
+
+INSERT INTO `krs_detail` (`id_krs_detail`, `id_krs`, `id_mk`) VALUES
+(4, 1, 1),
+(5, 1, 2),
+(6, 2, 3),
+(7, 3, 3),
+(8, 4, 5),
+(9, 5, 4),
+(10, 5, 5);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `mahasiswa`
+--
+
+CREATE TABLE `mahasiswa` (
+  `id_mahasiswa` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `nim` varchar(20) NOT NULL,
+  `prodi` varchar(50) DEFAULT NULL,
+  `angkatan` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `mahasiswa`
+--
+
+INSERT INTO `mahasiswa` (`id_mahasiswa`, `id_user`, `nim`, `prodi`, `angkatan`) VALUES
+(1, 4, '230000001', 'Informatika', 2023),
+(2, 5, '230000002', 'Sistem Informasi', 2023);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `mata_kuliah`
+--
+
+CREATE TABLE `mata_kuliah` (
+  `id_mk` int(11) NOT NULL,
+  `kode_mk` varchar(20) NOT NULL,
+  `nama_mk` varchar(100) NOT NULL,
+  `sks` int(11) NOT NULL,
+  `id_dosen` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `mata_kuliah`
+--
+
+INSERT INTO `mata_kuliah` (`id_mk`, `kode_mk`, `nama_mk`, `sks`, `id_dosen`) VALUES
+(1, 'IF101', 'Pemrograman Dasar', 3, 1),
+(2, 'IF201', 'Pemrograman OOP', 3, 1),
+(3, 'SI101', 'Pengantar Sistem Informasi', 2, 2),
+(4, 'IF303', 'Pengantar Semester 3', 144, 1),
+(5, 'IF501', 'Aljabar Geometri', 4, 2),
+(6, 'IF412', 'FiqriAcu', 4, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `nilai`
+--
+
+CREATE TABLE `nilai` (
+  `id_nilai` int(11) NOT NULL,
+  `id_mahasiswa` int(11) NOT NULL,
+  `id_mk` int(11) NOT NULL,
+  `nilai_angka` decimal(5,2) DEFAULT NULL,
+  `nilai_huruf` char(2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `nilai`
+--
+
+INSERT INTO `nilai` (`id_nilai`, `id_mahasiswa`, `id_mk`, `nilai_angka`, `nilai_huruf`) VALUES
+(4, 1, 1, 85.00, 'A'),
+(5, 1, 2, 78.00, 'B'),
+(6, 2, 3, 90.00, 'A');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `users`
+--
+
+CREATE TABLE `users` (
+  `id_user` int(11) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `role` enum('mahasiswa','dosen','admin') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `users`
+--
+
+INSERT INTO `users` (`id_user`, `nama`, `email`, `password`, `role`) VALUES
+(1, 'Admin Sistem', 'admin@sia.ac.id', 'admin123', 'admin'),
+(2, 'Pak Budi', 'budi@univ.ac.id', 'dosen123', 'dosen'),
+(3, 'Bu Sari', 'sari@univ.ac.id', 'dosen123', 'dosen'),
+(4, 'Andi', 'andi@mahasiswa.ac.id', 'mhs12345', 'mahasiswa'),
+(5, 'Rina', 'rina@mahasiswa.ac.id', 'mhs12345', 'mahasiswa');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id_admin`),
+  ADD UNIQUE KEY `username` (`username`),
+  ADD KEY `id_user` (`id_user`);
+
+--
+-- Indeks untuk tabel `dosen`
+--
+ALTER TABLE `dosen`
+  ADD PRIMARY KEY (`id_dosen`),
+  ADD UNIQUE KEY `nidn` (`nidn`),
+  ADD KEY `id_user` (`id_user`);
+
+--
+-- Indeks untuk tabel `krs`
+--
+ALTER TABLE `krs`
+  ADD PRIMARY KEY (`id_krs`),
+  ADD KEY `id_mahasiswa` (`id_mahasiswa`);
+
+--
+-- Indeks untuk tabel `krs_detail`
+--
+ALTER TABLE `krs_detail`
+  ADD PRIMARY KEY (`id_krs_detail`),
+  ADD KEY `id_krs` (`id_krs`),
+  ADD KEY `id_mk` (`id_mk`);
+
+--
+-- Indeks untuk tabel `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  ADD PRIMARY KEY (`id_mahasiswa`),
+  ADD UNIQUE KEY `nim` (`nim`),
+  ADD KEY `id_user` (`id_user`);
+
+--
+-- Indeks untuk tabel `mata_kuliah`
+--
+ALTER TABLE `mata_kuliah`
+  ADD PRIMARY KEY (`id_mk`),
+  ADD UNIQUE KEY `kode_mk` (`kode_mk`),
+  ADD KEY `id_dosen` (`id_dosen`);
+
+--
+-- Indeks untuk tabel `nilai`
+--
+ALTER TABLE `nilai`
+  ADD PRIMARY KEY (`id_nilai`),
+  ADD KEY `id_mahasiswa` (`id_mahasiswa`),
+  ADD KEY `id_mk` (`id_mk`);
+
+--
+-- Indeks untuk tabel `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id_user`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT untuk tabel `dosen`
+--
+ALTER TABLE `dosen`
+  MODIFY `id_dosen` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT untuk tabel `krs`
+--
+ALTER TABLE `krs`
+  MODIFY `id_krs` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT untuk tabel `krs_detail`
+--
+ALTER TABLE `krs_detail`
+  MODIFY `id_krs_detail` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT untuk tabel `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  MODIFY `id_mahasiswa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT untuk tabel `mata_kuliah`
+--
+ALTER TABLE `mata_kuliah`
+  MODIFY `id_mk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT untuk tabel `nilai`
+--
+ALTER TABLE `nilai`
+  MODIFY `id_nilai` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT untuk tabel `users`
+--
+ALTER TABLE `users`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `admin`
+--
+ALTER TABLE `admin`
+  ADD CONSTRAINT `admin_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`);
+
+--
+-- Ketidakleluasaan untuk tabel `dosen`
+--
+ALTER TABLE `dosen`
+  ADD CONSTRAINT `dosen_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`);
+
+--
+-- Ketidakleluasaan untuk tabel `krs`
+--
+ALTER TABLE `krs`
+  ADD CONSTRAINT `krs_ibfk_1` FOREIGN KEY (`id_mahasiswa`) REFERENCES `mahasiswa` (`id_mahasiswa`);
+
+--
+-- Ketidakleluasaan untuk tabel `krs_detail`
+--
+ALTER TABLE `krs_detail`
+  ADD CONSTRAINT `krs_detail_ibfk_1` FOREIGN KEY (`id_krs`) REFERENCES `krs` (`id_krs`),
+  ADD CONSTRAINT `krs_detail_ibfk_2` FOREIGN KEY (`id_mk`) REFERENCES `mata_kuliah` (`id_mk`);
+
+--
+-- Ketidakleluasaan untuk tabel `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  ADD CONSTRAINT `mahasiswa_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`);
+
+--
+-- Ketidakleluasaan untuk tabel `mata_kuliah`
+--
+ALTER TABLE `mata_kuliah`
+  ADD CONSTRAINT `mata_kuliah_ibfk_1` FOREIGN KEY (`id_dosen`) REFERENCES `dosen` (`id_dosen`);
+
+--
+-- Ketidakleluasaan untuk tabel `nilai`
+--
+ALTER TABLE `nilai`
+  ADD CONSTRAINT `nilai_ibfk_1` FOREIGN KEY (`id_mahasiswa`) REFERENCES `mahasiswa` (`id_mahasiswa`),
+  ADD CONSTRAINT `nilai_ibfk_2` FOREIGN KEY (`id_mk`) REFERENCES `mata_kuliah` (`id_mk`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
